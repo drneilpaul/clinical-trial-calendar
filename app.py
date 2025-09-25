e4b9460eb3de3011a202e7d9dd8faf0ae9ced90f
drneilpaul <neil.paul@nhs.net>
2025-09-25 09:15:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd  # Added this import
 from helpers import load_file, normalize_columns, parse_dates_column
 from processing_calendar import build_calendar
 from display_components import (
