78c2debac288a0ce205d4a5283c85eaf56aa1774
drneilpaul <neil.paul@nhs.net>
2026-01-10 20:51:17 +0000

    Add files via upload

--- modal_forms.py
+++ modal_forms.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import io
 from datetime import date, datetime, timedelta
+from typing import List, Tuple
 from helpers import load_file, log_activity, get_visit_type_series, trigger_data_refresh
 import database as db
 
